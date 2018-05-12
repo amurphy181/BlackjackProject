4afa810550ccb1e2fcf0314cbe6f55e1ba7ffbfd
Alex Murphy <alexander.f.murphy@gmail.com>
2018-05-12 13:16:15 -0600

    first round of broken aces fixed

--- src/com/skilldistillery/blackjack/BlackJackHand.java
+++ src/com/skilldistillery/blackjack/BlackJackHand.java
@@ -1,6 +1,8 @@
 package com.skilldistillery.blackjack;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -19,64 +21,87 @@ public class BlackJackHand {
 
 	public void run() {
 		Deck deck = new Deck();
-		List<Card> playerHand = new ArrayList<>();
+		List<Card> playerHand = new LinkedList<>();
 		List<Card> dealerHand = new ArrayList<>();
 		Hand actOnHands = new Hand();
+		boolean gameContinue = true;
+		Scanner input = new Scanner(System.in);
 
-		deck.shuffle();
-
-		playerHand.add(deck.dealCard());
-		dealerHand.add(deck.dealCard());
-		playerHand.add(deck.dealCard());
-		dealerHand.add(deck.dealCard());
-
-		int phCount = actOnHands.getHandValue(playerHand);
-		int dealerCount = actOnHands.getHandValue(dealerHand);
 		boolean handContinue = true;
 
-		if (dealerCount == 21) {
-			System.out.println("Dealer wins with a Blackjack!");
-			handContinue = false;
-		}
+		while (gameContinue) {
 
-		while (handContinue) {
-			Card dealerCardShowing = dealerHand.get(1);
-			System.out.println("Dealer is showing a " + dealerCardShowing);
+			deck.shuffle();
 
-			System.out.println("Player count: " + phCount + " with a hand of " + playerHand.get(0) + " and " + playerHand.get(1));
-			System.out.println("Hit or hold?");
-			Scanner input = new Scanner(System.in);
-			String userChoice = input.nextLine();
-			int totalCardsInHand = 2;
+			playerHand.add(deck.dealCard());
+			dealerHand.add(deck.dealCard());
+			playerHand.add(deck.dealCard());
+			dealerHand.add(deck.dealCard());
 
-			if ((userChoice.equals("Hit") || userChoice.equals("hit")) && (handContinue == true)) {
-				// check for going over on an ace if the current player hand count is more than 11
-				// this is the part that needs to be fixed! bugs all over here
-				if (phCount >= 11) {
-					playerHand.addAll(actOnHands.addCard());
-					Card lastCardDealt = playerHand.get(totalCardsInHand);
-					if (lastCardDealt.getRank() == Rank.ACE) {
-						phCount = phCount - 10;
-					}
-				} else {
-					playerHand.addAll(actOnHands.addCard());
-					phCount = actOnHands.getHandValue(playerHand);
-				}
+			int phCount = actOnHands.getHandValue(playerHand);
+			int dealerCount = actOnHands.getHandValue(dealerHand);
 
-				phCount = actOnHands.getHandValue(playerHand);
-				System.out.println(phCount);
-				if (phCount > 21) {
-					System.out.println("Oooooohhhhh busteddddd, dealer wins");
-					System.out.println("Dealer had a " + dealerCount);
-					handContinue = false;
-				}
-
-			} else if (userChoice.equals("Hold") || userChoice.equals("hold")) {
-				break;
+			if (dealerCount == 21 && dealerCount > phCount) {
+				System.out.println("Dealer wins with a Blackjack!");
+				handContinue = false;
+			} else if (dealerCount == 21 && phCount == 21) {
+				System.out.println("Two blackjacks, whoa! Push.");
+				handContinue = false;
+			} else if (phCount == 21) {
+				System.out.println("Player Blackjack wins!");
+				handContinue = false;
 			}
-		}
 
-			while (dealerCount < 17 && phCount <= 21) {
+			while (handContinue) {
+				Card dealerCardShowing = dealerHand.get(1);
+				System.out.println("Dealer is showing a " + dealerCardShowing);
+
+				System.out.println("Player count: " + phCount + "\nPlayer hand: " + playerHand.toString());
+				System.out.println("Hit or hold?");
+				String userChoice = input.nextLine();
+				int totalCardsInHand = 2;
+
+				if ((userChoice.equals("Hit") || userChoice.equals("hit")) && (handContinue == true)) {
+					// check for going over on an ace if the current player hand count is more than
+					// 11
+					// this is the part that needs to be fixed! bugs all over here
+					if (phCount >= 11) {
+						playerHand.addAll(actOnHands.addCard());
+						int aceTracker = phCount + 11;
+						phCount = actOnHands.getHandValue(playerHand);
+
+						totalCardsInHand++;
+						Card lastCardDealt = playerHand.get(totalCardsInHand - 1);
+						System.out.println("Card dealt was a : " + lastCardDealt.toString());
+						if ((phCount == aceTracker) && totalCardsInHand == 3) {
+
+							phCount = phCount - 10;
+						} else if ((phCount == aceTracker) && totalCardsInHand == 4) {
+							phCount = phCount - 20;
+						}
+						if (phCount == 21) {
+							System.out.println("Player has 21: will the dealer be able to match?");
+							break;
+						}
+					} else {
+						playerHand.addAll(actOnHands.addCard());
+						phCount = actOnHands.getHandValue(playerHand);
+					}
+
+					// phCount = actOnHands.getHandValue(playerHand);
+					System.out.println(phCount);
+					if (phCount > 21) {
+						System.out.println("Oooooohhhhh busteddddd, dealer wins");
+						System.out.println("Dealer had a " + dealerCount);
+						handContinue = false;
+					}
+
+				} else if (userChoice.equals("Hold") || userChoice.equals("hold")) {
+					break;
+				}
+			}
+
+			while (dealerCount < 17 && phCount <= 21 && handContinue) {
 				dealerHand.add(deck.dealCard());
 				dealerCount = actOnHands.getHandValue(dealerHand);
 				if (dealerCount > 21) {
@@ -84,20 +109,30 @@ public class BlackJackHand {
 
 				}
 			}
-			
+
 			if (phCount <= 21 && phCount > dealerCount) {
 				System.out.println("You win!");
 			} else if (dealerCount <= 21 && phCount < dealerCount) {
 				System.out.println("Dealer wins, womp");
-			} else if (phCount == dealerCount){
+			} else if (phCount == dealerCount) {
 				System.out.println("Push.");
 			}
 
-		
+			System.out.println("Dealer hand count: " + dealerCount);
+			System.out.println("Player's hand: " + playerHand.toString());
+			System.out.println("Dealer's hand: " + dealerHand.toString());
 
-		System.out.println("Dealer hand count: " + dealerCount);
-		System.out.println("Player's hand: " + playerHand.toString());
-		System.out.println("Dealer's hand: " + dealerHand.toString());
+			System.out.print("Play again? Yes or no: ");
+
+			String gameContinueChoice = input.nextLine();
+
+			if (gameContinueChoice == "No" || gameContinueChoice == "no") {
+				gameContinue = false;
+			} else {
+				continue;
+			}
+
+		}
 
 	}
 
